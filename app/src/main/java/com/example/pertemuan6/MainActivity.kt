b3c292cb7edb7d0dbe3095edf9590cfc07158e37
restiana <restianafp@gmail.com>
2024-10-07 04:59:00 +0700

    Add new line

--- app/src/main/java/com/example/pertemuan6/MainActivity.kt
+++ app/src/main/java/com/example/pertemuan6/MainActivity.kt
@@ -3,6 +3,9 @@ package com.example.pertemuan6
 import android.os.Bundle
 import android.view.View
 import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.DatePicker.OnDateChangedListener
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.pertemuan6.databinding.ActivityMainBinding
 import java.util.Calendar
@@ -26,17 +29,27 @@ class MainActivity : AppCompatActivity() {
             var selectedDate = "${_tempCalendar.get(Calendar.DAY_OF_MONTH)} ${monthList[_tempCalendar.get(Calendar.MONTH)]} ${_tempCalendar.get(Calendar.YEAR)}"
 
 
+//            Kehadiran Dropdown=======================================
+            val adapterKehadiran = ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_spinner_item,
+                kehadiranList
+            )
             kehadiranSpinner.adapter = adapterKehadiran
 
 //            Selected Kehadiran
             kehadiranSpinner.onItemSelectedListener =
                 object : AdapterView.OnItemSelectedListener {
                     override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+
                     }
 
                     override fun onNothingSelected(parent: AdapterView<*>?) {
+
                     }
                 }
+
+
 
         }
     }
